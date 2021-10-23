a711b8222d872b315ca30b7a8b5c6a1355df3d07
Thiago Billy <thiagobilly27@gmail.com>
2021-10-22 23:27:27 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const linkSocialMedia = {
+  github: 'TdB27',
+  youtube: 'channel/UCTYAROH_zl5JF9UNEtHtFrg',
+  facebook: 'thiago.billy.100',
+  instagram: 'thiagobilly27',
+  twitter: 'thiagobilly'
+}
+
+function changeSocialMedia() {
+  for (let li of socialMedia.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${linkSocialMedia[social]}`
+  }
+}
+
+changeSocialMedia()
+
+function getGithubProfileInfos() {
+  const url = `https://api.github.com/users/${linkSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+      console.log()
+    })
+}
+
+getGithubProfileInfos()
